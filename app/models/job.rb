5530a87a227ebb217ac9801e4ce6c1a6bd7f9137
Boony Guzman <josueguzman89@gmail.com>
2017-11-06 17:46:42 -0500

    11/6 6

--- app/models/job.rb
+++ app/models/job.rb
@@ -6,6 +6,6 @@ class Job < ApplicationRecord
 	has_and_belongs_to_many :boats, required: false
 
 	validates :description, presence: true, length: {minimum: 50}
-    validates :cost, numericality: { :greater_than => 1000, :less_than_or_equal_to => 999999999}
+  validates :cost, numericality: { :greater_than => 1000, :less_than_or_equal_to => 999999999}
 
 end
